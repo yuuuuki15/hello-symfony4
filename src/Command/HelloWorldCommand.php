9c79fb312ce2f42933793059736ba095a8e63861
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2023-04-04 16:36:06 +0200

    update: show user info

--- src/Command/HelloWorldCommand.php
+++ src/Command/HelloWorldCommand.php
@@ -33,40 +33,64 @@ class HelloWorldCommand extends Command
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+    // protected function execute(InputInterface $input, OutputInterface $output): int
+    // {
+    //     $io = new SymfonyStyle($input, $output);
+    //     $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+    //     if ($arg1) {
+    //         $io->note(sprintf('You passed an argument: %s', $arg1));
+    //     }
 
-        // if ($input->getOption('option1')) {
-        //     // ...
-        // }
+    //     // if ($input->getOption('option1')) {
+    //     //     // ...
+    //     // }
 
-        // $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+    //     // $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
         
-        $io->success('Hello World!');
+    //     $io->success('Hello World!');
 
-        // SymfonyStyleのインスタンスを作成する
-        $io = new SymfonyStyle($input, $output);
+    //     // SymfonyStyleのインスタンスを作成する
+    //     $io = new SymfonyStyle($input, $output);
 
-        // userテーブルのカラム名を取得する（例として、Doctrine ORMを使用する場合）
-        // $entityManager = $this->getContainer()->get('doctrine')->getManager();
-        // $entityManager = $this->getDoctrine()->getManager();
-        $userMetadata = $this->entityManager->getClassMetadata(User::class);
-        $columnNames = $userMetadata->getColumnNames();
+    //     // userテーブルのカラム名を取得する（例として、Doctrine ORMを使用する場合）
+    //     // $entityManager = $this->getContainer()->get('doctrine')->getManager();
+    //     // $entityManager = $this->getDoctrine()->getManager();
+    //     $userMetadata = $this->entityManager->getClassMetadata(User::class);
+    //     $columnNames = $userMetadata->getColumnNames();
 
-        // Tableヘルパークラスを使用して、カラム名を表示する
-        $table = new Table($io);
-        $table->setHeaders(['Column Name']);
-        $table->setRows(array_map(function($columnName) {
-            return [$columnName];
-        }, $columnNames));
-        $table->render();
+    //     // Tableヘルパークラスを使用して、カラム名を表示する
+    //     $table = new Table($io);
+    //     $table->setHeaders(['Column Name']);
+    //     $table->setRows(array_map(function($columnName) {
+    //         return [$columnName];
+    //     }, $columnNames));
+    //     $table->render();
 
-        return Command::SUCCESS;
+    //     return Command::SUCCESS;
+    // }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+{
+    $io = new SymfonyStyle($input, $output);
+
+    // Doctrine ORMのエンティティマネージャーを取得する
+    $entityManager = $this->entityManager;
+
+    // Userエンティティのリポジトリを取得する
+    $userRepository = $entityManager->getRepository(User::class);
+
+    // 上限3件までのユーザーデータを取得する
+    $users = $userRepository->findBy([], null, 3);
+
+    // テーブルヘルパーを使用して、取得したユーザーデータを表示する
+    $table = new Table($io);
+    $table->setHeaders(['id', 'email']);
+    foreach ($users as $user) {
+        $table->addRow([$user->getId(), $user->getEmail()]);
     }
+    $table->render();
+
+    return Command::SUCCESS;
+}
 }
